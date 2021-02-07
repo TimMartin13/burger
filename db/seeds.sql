f256e02ee8b670930ec17910012e4252b0034a3a
Mandelbrot <timmartin13@gmail.com>
2021-02-07 14:50:26 -0600

    Initial file setup

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Juicy Lucy", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Le Pigeon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Luger Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Animal Style", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Dyer's Deep-Fried Burger", false);
+
